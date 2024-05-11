464cc0b368315bb8c58dfea0a615befb634ba877
Khalisah Faroukh <khalshf@gmail.com>
2024-05-11 23:27:46 +0800

    small updates

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AboutmeComponent } from '../aboutme/aboutme.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [AboutmeComponent],
+  imports: [AboutmeComponent, FooterComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
